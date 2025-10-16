d7581d107c6f1e86647d12f501822d31313df874
leoproducts <masanja.developer@gmail.com>
2025-10-16 18:48:24 +0300

    endpoint

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,6 +1,6 @@
 // lib/config/config.dart
 class AppConfig {
-  static const String baseUrl = "http://23.92.65.72:8080";
+  static const String baseUrl = "https://api.stebofarm.co.tz";
   // static const String baseUrl = "http://10.0.2.2:8080"; 
 }
 
