a32cdf7d497e7f30bd2cd17c74c6910f0d2c1ced
leoproducts <masanja.developer@gmail.com>
2025-10-26 23:11:16 +0300

    update config

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -4,7 +4,5 @@ class AppConfig {
   static const String baseUrl = String.fromEnvironment(
     'APP_BASE_URL',
     defaultValue: 'https://api.stebofarm.co.tz',
-    // defaultValue: 'http://10.0.2.2:8080',
-    // defaultValue: 'http://localhost:8080',
   );
 }
