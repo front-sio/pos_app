406eca0434f3f064778ebdf908ab9b5e4faf0800
leoproducts <masanja.developer@gmail.com>
2025-11-14 11:16:08 +0300

    Update config

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -3,7 +3,7 @@ class AppConfig {
   // Base HTTP API URL (already used by your services)
   static const String baseUrl = String.fromEnvironment(
     'APP_BASE_URL',
-    defaultValue: 'https://magasin-api.iperfee.com',
+    defaultValue: 'https://app.stebofarm.co.tz',
   );
 }
 
@@ -12,3 +12,4 @@ class AppConfig {
 // defaultValue: 'https://api.stebofarm.co.tz',
 // defaultValue: 'https://magasin-api.iperfee.com',
 // defaultValue: 'http://74.50.97.22:8080',
+// defaultValue: 'https://app.stebofarm.co.tz',
